4ebbe71214bcf76c7cce7aa3565bbdfc2b2861c2
Wilf Silver <git@wilfsilver.co.uk>
2024-02-18 12:45:58 +0000

    Try make yarn install more reliable

--- dist/get-key-fn.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="node" />
-import { JwtVerifierOptions } from './jwt-verifier.js';
-export type JWKSOptions = Required<Pick<JwtVerifierOptions, 'cooldownDuration' | 'timeoutDuration' | 'cacheMaxAge'>> & Pick<JwtVerifierOptions, 'agent' | 'secret'>;
-declare const _default: ({ agent, cooldownDuration, timeoutDuration, cacheMaxAge, secret, }: JWKSOptions) => (jwksUri: string) => ((protectedHeader?: import("jose").JWSHeaderParameters | undefined, token?: import("jose").FlattenedJWSInput | undefined) => Promise<import("jose").KeyLike>) | (() => import("crypto").KeyObject);
-export default _default;
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-export { default as jwtVerifier, } from './jwt-verifier.js';
-export { InvalidTokenError, UnauthorizedError, InsufficientScopeError, } from 'oauth2-bearer';
-export { default as discover } from './discovery.js';
-export { claimCheck, claimEquals, claimIncludes, requiredScopes, scopeIncludesAny, } from './claim-check.js';
